b9b17dabab2cdfd6308981018b073a6543c29a58
Zaw <zawlinnnaing0018@gmail.com>
2023-08-01 00:04:32 +0700

    feat(markdown-preview): init commit

---
+++ markdown-preview/go.mod
@@ -0,0 +1,14 @@
+module github.com/zawlinnnaing/go-clis/markdown-preview
+
+go 1.20
+
+require (
+	github.com/microcosm-cc/bluemonday v1.0.25
+	github.com/russross/blackfriday/v2 v2.1.0
+)
+
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+)
---
+++ markdown-preview/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	header = `
+	<!DOCTYPE html>
+	<html> <head> <meta http-equiv="content-type" content="text/html; charset=utf-8"> <title>Markdown Preview Tool</title> </head> <body>`
+	footer = `
+		</body>
+	</html>
+	`
+)
+
+func main() {
+	filename := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
+
+func run(filename string) error {
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
+
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+	var buffer bytes.Buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+	return buffer.Bytes()
+}
+
+func saveHTML(filename string, data []byte) error {
+	return ioutil.WriteFile(filename, data, 0644)
+}
