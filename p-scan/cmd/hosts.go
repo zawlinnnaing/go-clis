c594ea02439e03e2081ad881216cd880f05cda5a
Zaw <zawlinnnaing0018@gmail.com>
2023-10-08 01:08:31 +0700

    feat(p-scan): added hosts sub-command

---
+++ p-scan/cmd/hosts.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// hostsCmd represents the hosts command
+var hostsCmd = &cobra.Command{
+	Use:   "hosts",
+	Short: "Manage the hosts list",
+	Long: `	Manage hosts list.
+
+	- Add hosts with add commend
+	- Delete hosts with delete command
+	- List hosts with list command
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hosts called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(hostsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// hostsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// hostsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
