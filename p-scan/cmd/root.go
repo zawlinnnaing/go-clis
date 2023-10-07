a86b9b3e4509acef0076688c6239fc94b694ef83
Zaw <zawlinnnaing0018@gmail.com>
2023-10-08 01:03:20 +0700

    chore(p-scan): init program

---
+++ p-scan/cmd/root.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "p-scan",
+	Short: "Fast TCP port scanner",
+	Long: `p-scan -- short for Port Scanner -- executes TCP port scan on a list of hosts.
+	p-scan allows you to add, list, and delete hosts from the list.
+	p-scan executes a port scan on specified TCP ports.
+	You can customize the target ports using a command line flag.
+	REMEMBER: Please only execute this program against the system you own.
+	`,
+	Version: "0.1",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.p-scan.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
---
+++ p-scan/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/zawlinnnaing/go-clis/p-scan/cmd"
+
+func main() {
+	cmd.Execute()
+}
